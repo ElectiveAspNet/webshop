e3c1a78bb210ecf0dd4fb0f53f026af12b948b43
Claus Bove <clbo@kea.dk>
2017-10-02 11:27:54 +0200

    products route added

--- Webshop/Startup.cs
+++ Webshop/Startup.cs
@@ -48,7 +48,7 @@ namespace Webshop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Product}/{action=Index}/{id?}");
+                    template: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
