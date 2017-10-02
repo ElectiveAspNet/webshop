665c972327ac669107511b99926f76f2c4db82d7
Claus Bove <clbo@kea.dk>
2017-10-02 10:58:44 +0200

    initial

---
+++ Webshop/Data/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Webshop.Models.Entities;
+
+namespace Webshop.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+
+        public MyDbContext (DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
